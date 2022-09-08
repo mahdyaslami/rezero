4302f8855172dc7df594191aa89c4a1a068c2170
mahdyaslami <mahdyaslami@gmail.com>
2022-09-08 07:52:51 +0430

    Update icons

--- resources/js/Jetstream/Welcome.vue
+++ resources/js/Jetstream/Welcome.vue
@@ -1,5 +1,8 @@
 <script setup>
 import JetApplicationLogo from '@/Jetstream/ApplicationLogo.vue';
+import IconTote from '@/Components/IconTote.vue';
+import IconPassword from '@/Components/IconPassword.vue';
+import IconArrowLeft from '@/Components/IconArrowLeft.vue';
 </script>
 
 <template>
@@ -23,11 +26,7 @@ import JetApplicationLogo from '@/Jetstream/ApplicationLogo.vue';
         <div class="bg-gray-200 bg-opacity-25 grid grid-cols-1 md:grid-cols-2">
             <div class="p-6">
                 <div class="flex items-center">
-                    <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
-                        stroke-width="2" viewBox="0 0 24 24" class="w-8 h-8 text-gray-400">
-                        <path
-                            d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253" />
-                    </svg>
+                    <IconTote class="w-8 h-8" fill="gray" />
                     <div class="ltr:ml-4 rtl:mr-4 text-lg text-gray-600 leading-7 font-semibold">
                         <a href="https://laravel.com/docs">Tote</a>
                     </div>
@@ -46,11 +45,7 @@ import JetApplicationLogo from '@/Jetstream/ApplicationLogo.vue';
                             <div>کاوش در برنامه</div>
 
                             <div class="ltr:ml-1 rtl:mr-1 text-indigo-500">
-                                <svg viewBox="0 0 20 20" fill="currentColor" class="w-4 h-4">
-                                    <path fill-rule="evenodd"
-                                        d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-                                        clip-rule="evenodd" />
-                                </svg>
+                                <IconArrowLeft class="w-5 h-5" fill="blue" />
                             </div>
                         </div>
                     </a>
@@ -59,12 +54,7 @@ import JetApplicationLogo from '@/Jetstream/ApplicationLogo.vue';
 
             <div class="p-6 border-t border-gray-200 md:border-t-0 md:border-l">
                 <div class="flex items-center">
-                    <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
-                        stroke-width="2" viewBox="0 0 24 24" class="w-8 h-8 text-gray-400">
-                        <path
-                            d="M3 9a2 2 0 012-2h.93a2 2 0 001.664-.89l.812-1.22A2 2 0 0110.07 4h3.86a2 2 0 011.664.89l.812 1.22A2 2 0 0018.07 7H19a2 2 0 012 2v9a2 2 0 01-2 2H5a2 2 0 01-2-2V9z" />
-                        <path d="M15 13a3 3 0 11-6 0 3 3 0 016 0z" />
-                    </svg>
+                    <IconPassword class="w-8 h-8" fill="gray" />
                     <div class="ltr:ml-4 rtl:mr-4 text-lg text-gray-600 leading-7 font-semibold">
                         <a href="https://laracasts.com">Passwords</a>
                     </div>
@@ -81,11 +71,7 @@ import JetApplicationLogo from '@/Jetstream/ApplicationLogo.vue';
                             <div>کاوش در برنامه</div>
 
                             <div class="ltr:ml-1 rtl:mr-1 text-indigo-500">
-                                <svg viewBox="0 0 20 20" fill="currentColor" class="w-4 h-4">
-                                    <path fill-rule="evenodd"
-                                        d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-                                        clip-rule="evenodd" />
-                                </svg>
+                                <IconArrowLeft class="w-5 h-5" fill="blue" />
                             </div>
                         </div>
                     </a>
