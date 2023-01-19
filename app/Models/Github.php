749ceeccbac094b8fefe1771b0b55064dd1b93ab
mahdyaslami <mahdyaslami@gmail.com>
2023-01-19 23:07:34 +0330

    Add Github::api() method

--- app/Models/Github.php
+++ app/Models/Github.php
@@ -20,4 +20,14 @@ class Github extends Model
     {
         return $this->belongsTo(User::class);
     }
+
+    public function api()
+    {
+        $client = app('github.api');
+        $client->authenticate(
+            $this->token,
+            \Github\AuthMethod::ACCESS_TOKEN
+        );
+        return $client;
+    }
 }
