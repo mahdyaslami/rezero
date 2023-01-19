cf14f20338d7fe391c40edfa7c869830364a8e51
mahdyaslami <mahdyaslami@gmail.com>
2023-01-19 11:41:16 +0330

    Add storing a note

---
+++ app/Http/Controllers/NoteController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\NoteRequest;
+use Inertia\Inertia;
+
+class NoteController extends Controller
+{
+    public function index()
+    {
+        return Inertia::render('Note', [
+            'notes' => auth()->user()->notes,
+        ]);
+    }
+
+    public function store(NoteRequest $request)
+    {
+        auth()->user()->notes()->create($request->validated());
+    }
+}
