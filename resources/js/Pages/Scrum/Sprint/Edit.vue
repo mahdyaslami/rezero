38ced4e2769e69d9ec49a677d3dbb8889702bccd
mahdyaslami <mahdyaslami@gmail.com>
2023-01-24 21:08:49 +0330

    Show github oauth when user edit sprint

--- resources/js/Pages/Scrum/Sprint/Edit.vue
+++ resources/js/Pages/Scrum/Sprint/Edit.vue
@@ -3,6 +3,7 @@
 import AppLayout from '@/Layouts/AppLayout.vue'
 import AppSelect from '@/Components/Select.vue'
 import { ref } from 'vue'
+import AuthenticateGithubApp from './Partials/AuthenticateGithubApp.vue'
 
 const props = defineProps({
   oauth: {
@@ -21,16 +22,16 @@ const selected = ref(props.repos[0])
 <template>
   <AppLayout title="New Sprint">
     <div class="max-w-7xl mx-auto py-10 sm:px-6 lg:px-8">
-      <div v-show="!oauth" class="mb-5 text-blue-500">
-        شما پیش از استفاده از این ابزار نیاز دارید تا برنامه ما را در گیت هاب خود
-        احراز هویت کنید. این کار را می تواند از طریق بخش کاربری انجام دهید.
-      </div>
+      <template v-if="oauth">
+        <AppSelect
+          v-model="selected"
+          value="name"
+          class="w-60"
+          :list="repos"
+        />
+      </template>
 
-      <AppSelect
-        v-model="selected"
-        value="name"
-        :list="repos"
-      />
+      <AuthenticateGithubApp v-else />
     </div>
   </AppLayout>
 </template>
