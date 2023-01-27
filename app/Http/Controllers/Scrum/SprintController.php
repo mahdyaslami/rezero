86a1a1097769b447cd4ffd3febab9955f0820bd5
mahdyaslami <mahdyaslami@gmail.com>
2023-01-27 09:32:47 +0330

    Improve SprintController

--- app/Http/Controllers/Scrum/SprintController.php
+++ app/Http/Controllers/Scrum/SprintController.php
@@ -2,9 +2,8 @@
 
 namespace App\Http\Controllers\Scrum;
 
+use App\Facades\Github;
 use App\Http\Controllers\Controller;
-use Illuminate\Support\Arr;
-use Illuminate\Support\Facades\Cache;
 use Inertia\Inertia;
 
 class SprintController extends Controller
@@ -35,26 +34,9 @@ class SprintController extends Controller
 
         if (!!auth()->user()->github) {
             $oauth = true;
-            $repos = $this->repos();
+            $repos = Github::repositories();
         }
 
         return Inertia::render('Scrum/Sprint/Edit', compact('oauth', 'repos'));
-    }
-
-    private function repos()
-    {
-        $client = app('github.api');
-        $client->authenticate(
-            auth()->user()->github->token,
-            \Github\AuthMethod::ACCESS_TOKEN
-        );
-
-        $result = Cache::remember(
-            auth()->user()->github->username . ':repos',
-            300,
-            fn () => $client->api('me')->repositories()
-        );
-        $result = collect($result);
-        return $result->map(fn ($r) =>  Arr::only($r, ['id', 'name']));
     }
 }
