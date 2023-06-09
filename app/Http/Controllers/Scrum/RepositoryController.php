128a7dcd74df83c704bed234d087f0ffe525f6b2
mahdyaslami <mahdyaslami@gmail.com>
2023-06-09 18:26:10 +0430

    Move RepositoryRequest to Scrum namespace

--- app/Http/Controllers/Scrum/RepositoryController.php
+++ app/Http/Controllers/Scrum/RepositoryController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Scrum;
 
 use Facades\App\Github;
 use App\Http\Controllers\Controller;
-use App\Http\Requests\RepositoryRequest;
+use App\Http\Requests\Scrum\RepositoryRequest;
 use Inertia\Inertia;
 
 class RepositoryController extends Controller
--- app/Http/Requests/Scrum/RepositoryRequest.php
+++ app/Http/Requests/Scrum/RepositoryRequest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Requests;
+namespace App\Http\Requests\Scrum;
 
 use Illuminate\Foundation\Http\FormRequest;
 
