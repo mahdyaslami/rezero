770ff9275551fad7e1999d15e19e4a98c9e6ec60
Mahdi Aslami Khavari <mahdyaslami@gmail.com>
2023-11-14 15:01:00 +0330

    Add DictionaryRequest

--- app/Http/Controllers/DictionaryController.php
+++ app/Http/Controllers/DictionaryController.php
@@ -2,16 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\DictionaryRequest;
 use App\Models\Dictionary;
 use Illuminate\Http\Request;
 
 class DictionaryController extends Controller
 {
-    public function store()
+    public function store(DictionaryRequest $request)
     {
         Dictionary::updateOrCreate(
-            ['key' => request()->get('key')],
-            ['value'=> request()->get('value')]
+            ['key' => $request->get('key')],
+            ['value'=> $request->get('value')]
         );
     }
 }
